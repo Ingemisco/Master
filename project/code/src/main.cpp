334c1e06f2d332de8e1cda1dd32e369ee231cfe9
Christian Abdullahad <christian.abdullahad@hotmail.de>
2025-01-29 14:28:39 +0100

    add parsing of command line inputs using boost

--- project/code/src/main.cpp
+++ project/code/src/main.cpp
@@ -1,6 +1,25 @@
+#include <boost/program_options.hpp>
+#include <boost/program_options/detail/parsers.hpp>
+#include <boost/program_options/options_description.hpp>
+#include <boost/program_options/variables_map.hpp>
+#include <iostream>
 
+namespace po = boost::program_options;
 
-static void make_options_description(int argc, char *argv[]) {}
+static void make_options_description(int argc, char *argv[]) {
+  po::options_description description("Allowed options");
+  description.add_options()           //
+      ("help,h", "Show help message") //
+      ;
+
+  po::variables_map map;
+  po::store(po::parse_command_line(argc, argv, description), map);
+
+  if (map.count("help")) {
+    std::cout << description;
+    exit(0);
+  }
+}
 
 int main(int argc, char *argv[]) {
   make_options_description(argc, argv);
