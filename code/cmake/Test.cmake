5c1f0ab3189c747c287754cdfaf9b2485317356e
Christian Abdullahad <christian.abdullahad@hotmail.de>
2025-07-22 18:22:06 +0200

    add testing setup

--- code/cmake/Test.cmake
+++ code/cmake/Test.cmake
@@ -1,25 +1,15 @@
+message(STATUS "Test mode enabled")
+
+set(DEBUG_VALUE 0)
 add_compile_options(
-  -O0 
-  -g 
-  -fsanitize=address 
-  -fsanitize=undefined 
-  -fno-omit-frame-pointer
+  -O3 
+  -DNDEBUG
 )
-
-if(CMAKE_CXX_COMPILER_ID STREQUAL "Clang")
-    add_compile_options(-Wweak-vtables)
-endif()
-
-set(DEBUG_VALUE 1)
-file(GLOB TEST_SRC_FILES "test/*.cpp" "src/datastructures.cpp" "src/distance.cpp")
-
-add_executable(test_algorithms ${TEST_SRC_FILES})
 
 find_package(Boost REQUIRED COMPONENTS unit_test_framework)
-target_link_libraries(test_algorithms PRIVATE 
-  -fsanitize=address  
-  -fsanitize=undefined
-  Boost::headers 
-  Boost::unit_test_framework
-)
+
+enable_testing()
+
+add_subdirectory(${CMAKE_SOURCE_DIR}/tests)
+
 
---
+++ code/tests/general_functions.cpp
@@ -0,0 +1,40 @@
+#include <cmath>
+#define BOOST_TEST_MODULE GeneralTests
+
+#include <boost/test/tools/old/interface.hpp>
+#include <boost/test/unit_test_suite.hpp>
+#include <boost/test/unit_test.hpp>
+
+#include "distance.h"
+#include "datastructures.h"
+
+using DataStructures::integer_exponentiation;
+using DataStructures::Polyline;
+using DataStructures::unnormalized_euclidean_distance;
+using DataStructures::euclidean_distance;
+using DataStructures::manhattan_distance;
+using DataStructures::chebyshev_distance;
+using DataStructures::unnormalized_minkowski_distance;
+using DataStructures::minkowski_distance;
+
+BOOST_AUTO_TEST_CASE(IntegerExponentiation) {
+	BOOST_CHECK_EQUAL(integer_exponentiation(2,5), 32);
+	BOOST_CHECK_EQUAL(integer_exponentiation(3,4), 81);
+	BOOST_CHECK_EQUAL(integer_exponentiation(1.5f, 2), 2.25f); // both numbers can be represented with no inaccuracies so equal check should work
+}
+
+BOOST_AUTO_TEST_CASE(PointDistances) {
+	auto p = Polyline::from_file("../test_data/p0");
+	auto &polyline = *p;
+
+	BOOST_CHECK_CLOSE(unnormalized_euclidean_distance(polyline, 0, 1), 5, 0);
+	BOOST_CHECK_CLOSE(manhattan_distance(polyline, 0, 1), 3, 0);
+	BOOST_CHECK_CLOSE(chebyshev_distance(polyline, 0, 1), 2, 0);
+
+	BOOST_CHECK_CLOSE(euclidean_distance(polyline, 0, 1), std::sqrt(5), 1e-2);
+	BOOST_CHECK_CLOSE(unnormalized_minkowski_distance(polyline, 0, 1, 2), 5, 0);
+	BOOST_CHECK_CLOSE(unnormalized_minkowski_distance(polyline, 0, 1, 4), 17, 0);
+
+	BOOST_CHECK_CLOSE(minkowski_distance(polyline, 0, 1, 2), sqrt(5), 1e-2);
+	BOOST_CHECK_CLOSE(minkowski_distance(polyline, 0, 1, 4), std::pow(17, 0.25), 1e-2);
+}
